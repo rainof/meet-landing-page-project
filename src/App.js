42ef3dcc3129e9abdeffc4e93688faaf5cb926d1
rainof <13416303+rainof@users.noreply.github.com>
2024-08-03 00:06:02 +0700

    feat: convey data and setup structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import './App.scss';
+import logo_img from './assets/logo.svg'
 
-function App() {
+function App({data}) {
+  const { quote, description, download_button, what_button, section_1, section_1_title, section_1_quote, section_1_description, section_2, section_2_title, section_2_description } = data;
+
   return (
-    <div>
+    <div className="app">
+
+      <header>
+        <img className="logo" src={logo_img} alt="logo" />
+        <div className="hero_faces"></div>
+        <p className="app_quote">{quote}</p>
+        <p className="app_desc">{description}</p>
+        <button className="download_btn">{download_button}</button>
+        <button className="what_btn">{what_button}</button>
+      </header>
+
+      <main>
+        <p className="section_icon">{section_1}</p>
+        <p className="sect_1_title">{section_1_title}</p>
+        <p className="sect_1_quote">{section_1_quote}</p>
+        <p className="sect_1_desc">{section_1_description}</p>
+      </main>
+
+      <footer>
+        <p className="section_icon">{section_2}</p>
+        <p className="sect_2_title">{section_2_title}</p>
+        <p className="sect_2_desc">{section_2_description}</p>
+      </footer>
 
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -5,9 +5,24 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const appData = {
+  quote: "Group Chat for Everyone",
+  description: "Meet makes it easy to connect with others face-to-face virtually and collaborate across any device.",
+  download_button: "Download v1.3",
+  what_button: "What is it?",
+  section_1: "01",
+  section_1_title: "Built for modern use",
+  section_1_quote: "Smarter meetings, all in one place",
+  section_1_description: "Send messages, share files, show your screen, and record your meetings — all in one workspace. Control who can join with invite-only team access, data encryption, and data export.",
+  section_2: "02",
+  section_2_title: "Experience more together",
+  section_2_description: "Stay connected with reliable HD meetings and unlimited one-on-one and group video sessions."
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App data={appData} />
   </React.StrictMode>
 );
 
